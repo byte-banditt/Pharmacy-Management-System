3c4b29472ae031fe409b5d3f9a31dfb36e2101da
byte-banditt <ayusshtayde05@gmail.com>
2025-04-24 03:40:09 +0530

    fixed prescription

--- procedures/addPres.sql
+++ procedures/addPres.sql
@@ -2,17 +2,11 @@ CREATE OR REPLACE PROCEDURE AddPrescription(
     p_doctorid      IN VARCHAR2,
     p_patientid     IN VARCHAR2,
     p_date          IN DATE, -- Use appropriate date type if needed
-    p_no_of_drugs   IN NUMBER
+    p_presc_id      OUT NUMBER
 ) AS
-DECLARE
-    v_limit NUMBER := p_no_of_drugs;
 BEGIN
-    INSERT INTO Prescription (DoctorID, PatientID, Date_of_pres)VALUES (p_doctorid, p_patientid, p_date);
-    FOR i IN 1..v_limit LOOP
-        AddPrescriptionDrug()
-    END LOOP;
-END;
-/
-
+    INSERT INTO Prescription (DoctorID, PatientID, Date_of_pres)
+    VALUES (p_doctorid, p_patientid, p_date)
+    RETURNING PrescriptionID INTO p_presc_id;
 END;
 /
